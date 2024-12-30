10702c1073a9f90d9f929a2c43d52dd64f2fadce
Lucifer-davi <158458730+Lucifer-davi@users.noreply.github.com>
2024-12-30 18:00:37 +0530

    If you want to commit in my file you also add your file repo.
    Here's the complete source code of my Second project. The first page is (index.html). If you want to add some new features and some updates i suggest add your complete neat and clean source code.

---
+++ file.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <form action="welcome.php"
+        method="post">
+        Name:<input type="text"
+        name="name"><br>
+        Phone number:<"input type=number"
+        name="Phone number"><br>
+        E-Mail:<input type="text"
+        name="E-Mail"><br>
+        </form>
+    </body>
+</html>
+
+    
